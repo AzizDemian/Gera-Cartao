582e71f9d45d43cf204296d6a655dbf7a98d00f4
AzizDemian <106287944+AzizDemian@users.noreply.github.com>
2026-02-20 11:47:39 -0300

    Update atividade.c

--- atividade.c
+++ atividade.c
@@ -1,58 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main() {
 
-    char cartao[12];
-    int verificador1, verificador2, ii;
+    /*
+    ATIVIDADE:
 
-    srand(1); // Semente fixa para correção automática
+    Gere e imprima um número de cartão no seguinte formato:
 
-    int auxbanco = rand()%3;
+    XXXX-XXXX-XXXX-DD
 
-    if(auxbanco == 0) {
-        cartao[0]='5'; cartao[1]='1'; cartao[2]='4'; cartao[3]='7';
-    } else if(auxbanco == 1) {
-        cartao[0]='8'; cartao[1]='9'; cartao[2]='1'; cartao[3]='4';
-    } else {
-        cartao[0]='6'; cartao[1]='8'; cartao[2]='6'; cartao[3]='5';
-    }
+    Onde:
+    - Cada X representa um dígito numérico (0 a 9).
+    - DD representa dois dígitos verificadores (também de 0 a 9).
+    - Os grupos devem ser separados por hífen (-).
+    - O total deve conter exatamente 16 dígitos e 3 hífens.
+    - O formato deve ser exatamente como mostrado, sem espaços extras.
 
-    for(ii=4; ii<=11; ii++)
-        cartao[ii] = (rand()%10) + 48;
+    REGRAS IMPORTANTES:
 
-    // === cálculo do verificador1 ===
-    verificador1=0;
-    for(ii=0;ii<=3;ii++)
-        verificador1 += (5-ii)*(cartao[ii]-48);
+    1) NÃO imprimir mensagens adicionais como:
+       - "Seu cartão é:"
+       - "Número gerado:"
+       - Qualquer texto explicativo
 
-    for(ii=4;ii<=11;ii++)
-        verificador1 += (13-ii)*(cartao[ii]-48);
+       A saída deve conter APENAS o cartão no formato especificado.
 
-    verificador1 %= 11;
-    if(verificador1<2) verificador1=0;
-    else verificador1=11-verificador1;
+    2) NÃO utilizar:
+       - Loop infinito (ex: while(1), for(;;))
+       - goto
+       - system()
+       - bibliotecas além de stdio.h
 
-    // === cálculo do verificador2 ===
-    verificador2=0;
-    for(ii=0;ii<=4;ii++)
-        verificador2 += (6-ii)*(cartao[ii]-48);
+    3) O programa deve:
+       - Compilar sem erros
+       - Executar normalmente
+       - Finalizar corretamente (sem travar)
 
-    for(ii=5;ii<=11;ii++)
-        verificador2 += (14-ii)*(cartao[ii]-48);
+    4) NÃO solicitar entrada do usuário.
 
-    verificador2 += 2*verificador1;
-    verificador2 %= 11;
+    5) A saída deve ser exibida em uma única linha.
 
-    if(verificador2<2) verificador2=0;
-    else verificador2=11-verificador2;
+    Exemplo de saída válida:
+    1234-5678-9012-34
 
-    printf("%c%c%c%c-%c%c%c%c-%c%c%c%c-%d%d\n",
-           cartao[0],cartao[1],cartao[2],cartao[3],
-           cartao[4],cartao[5],cartao[6],cartao[7],
-           cartao[8],cartao[9],cartao[10],cartao[11],
-           verificador1, verificador2);
+    */
 
     return 0;
 }
